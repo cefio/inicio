ecfcbca04d472cb6eaa0518c4098f2054085d4ee
cefio <35591284+cefio@users.noreply.github.com>
2018-09-25 15:13:50 -0500

    Update funcion.php

--- funcion.php
+++ funcion.php
@@ -1,10 +1,3 @@
 <?php
-// Abrir el archivo
-$archivo = 'temp.txt';
-$codigo = $_POST['codigo'];
-// Guardar Archivo
-$abrir = fopen($archivo,'w');
-fwrite($abrir,$contenido);
-fclose($abrir);
- echo $codigo;
+ echo "ola";
 ?>
