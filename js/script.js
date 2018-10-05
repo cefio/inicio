4dcf2996fb68a48de7a20a6fb130b06ed07d2dd7
cefio <35591284+cefio@users.noreply.github.com>
2018-10-04 22:48:21 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $("#button").click(function() {
+        $('.transform').toggleClass('sombraup');
+        $('.titulo').toggleClass('aparecer');
+        $('.dino').toggleClass('aparece');
+      });
+ 
+ });
